2e6249775980febed0f3fd43b3eee8409bc16ae8
Ethanlool <prrooooos@gmail>
2022-11-17 08:41:37 -0600

    sjhdv

---
+++ crip/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCf4-STUxntxv-5a9tDIPTxSz8gUsSbH54",
+  authDomain: "pene-488df.firebaseapp.com",
+  projectId: "pene-488df",
+  storageBucket: "pene-488df.appspot.com",
+  messagingSenderId: "793639890018",
+  appId: "1:793639890018:web:1d6f8923db16f2c4a6753b",
+  measurementId: "G-R0QQQQJ4KD"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+  
